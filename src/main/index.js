4e9c147a4ba6ca6d6f197d5aea84ee8990606b6b
jaehokim12 <62605981+jaehokim12@users.noreply.github.com>
2021-04-21 14:54:18 +0900

    수정

--- src/main/index.js
+++ src/main/index.js
@@ -1,73 +1,121 @@
 import "./index.css";
-import imagesbanner from "../images/banners/banner1.png"
-import logo from "../images/icons/logo.png"
-import product1 from "../images/products/keyboard1.jpg"
-import product2 from "../images/products/basketball1.jpeg"
+
 import axios from "axios";
-import React from 'react';
 
-
+import React from "react";
 
 
 
 function MainPage() {
 
-    const [products, setproducts] =React.useState([]);
-    React.useEffect(
-      function(){
+  const [products, setProducts] = React.useState([]);
 
-        axios.get("https://c555926e-c811-4837-a109-3a81c0b97a7c.mock.pstmn.io/products")
-        .then(function(result){
-          
-          const products = result.data.products;
-          setproducts(products);
-            console.log(result);
-        }).catch(function(error){
-            console.error("에러발생 :", error);
-        });
-      },[])
+  React.useEffect(function () {
+
+    axios
+
+      .get(
+
+        "https://c555926e-c811-4837-a109-3a81c0b97a7c.mock.pstmn.io/products"
+
+      )
+
+      .then(function (result) {
+
+        const products = result.data.products;
+
+        setProducts(products);
+
+      })
+
+      .catch(function (error) {
+
+        console.error("에러 발생 : ", error);
+
+      });
+
+  }, []);
 
   return (
+
     <div>
+
       <div id="header">
+
         <div id="header-area">
-          <img src={logo}/>
+
+          <img src="images/icons/logo.png" />
+
         </div>
+
       </div>
+
       <div id="body">
+
         <div id="banner">
-           <img src={imagesbanner}/>
+
+          <img src="images/banners/banner1.png" />
+
         </div>
+
         <h1>판매되는 상품들</h1>
+
         <div id="product-list">
-          {
-            products.map(function(product, index){
-              return (
-                <div className="product-card">
-            <div>
-              <img
-                className="product-img"
-                src={product.imageUrl}
-              />
-            </div>
-            <div className="product-contents">
-              <span className="product-name">{product.name}</span>
-              <span className="product-price">{product.price}원</span>
-              <div className="product-seller">
-                <img className="product-avatar" src="images/icons/avatar.png" />
-                <span>{product.seller}</span>
+
+          {products.map(function (product, index) {
+
+            return (
+
+              <div className="product-card">
+
+                <div>
+
+                  <img className="product-img" src={product.imageUrl} />
+
+                </div>
+
+                <div className="product-contents">
+
+                  <span className="product-name">{product.name}</span>
+
+                  <span className="product-price">{product.price}원</span>
+
+                  <div className="product-seller">
+
+                    <img
+
+                      className="product-avatar"
+
+                      src="images/icons/avatar.png"
+
+                    />
+
+                    <span>{product.seller}</span>
+
+                  </div>
+
+                </div>
+
               </div>
-            </div>
-          </div>
-              )
-            })
-          }
-          
+
+            );
+
+          })}
+
         </div>
+
       </div>
+
+
+
       <div id="footer"></div>
+
     </div>
+
   );
+
 }
+
+
 
 export default MainPage;
